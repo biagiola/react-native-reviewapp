455030a27b995634104b71969f47eba3137d7791
Marcelo <marcelobiagiola01@gmail.com>
2020-09-15 16:54:33 -0400

    navigation modified

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
-import { globalStyles } from '../styles/globalStyles'
+import { View, Text, } from 'react-native'
+import { globalStyles } from '../styles/global'
 
 export default function ReviewDetails({ navigation }) {
-
-  const pressHandler = () => {
-    navigation.goBack()
-  }
-
   return (
     <View style={globalStyles.container}>
-      <Text>Review Details Screen</Text>
-      <Button title='back to home screen' onPress={ pressHandler } />
+      <Text style={globalStyles.titleText}>{ navigation.getParam('title')}</Text>
+      <Text style={globalStyles.titleText}>{ navigation.getParam('body')}</Text>
+      <Text style={globalStyles.titleText}>{ navigation.getParam('rating')}</Text>
     </View>
   )
 }
