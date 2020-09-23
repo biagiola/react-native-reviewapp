f7d8742c9398feb7177d3b1fda1e158191509940
Marcelo <marcelobiagiola01@gmail.com>
2020-09-23 10:02:40 -0400

    custom button component

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,6 +3,7 @@ import { StyleSheet, Button, TextInput, View, Text } from 'react-native'
 import { globalStyles } from '../styles/global'
 import { Formik } from 'formik'
 import * as yup from 'yup'
+import FlatButton from '../shared/button'
 
 const reviewSchema = yup.object({
   title: yup.string().required().min(4),
@@ -57,7 +58,7 @@ export default function ReviewForm({ addReview }) {
             />
             <Text style={globalStyles.errorText}>{ props.touched.rating && props.errors.rating }</Text>
 
-            <Button id='submit' name='submit' title='submit' color='salmon' onPress={props.handleSubmit} />
+            <FlatButton text='submit' onPress={props.handleSubmit} />
           </View>
         )}
 
